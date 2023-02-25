806786cb1107155310b244e8ea97af53e397deab
Andreyfarmik <gemefarmik@yandex.ru>
2023-02-26 01:25:44 +0500

    Задача 12 готова

---
+++ Python code/Task12.py
@@ -0,0 +1,10 @@
+x =int(input("Введите сумму натуральных число S: "))
+y =int(input("Введите произведение натуральных число P: "))
+if x and y >= 1000:
+    print('Условия задачи нашены!!! Введите числа до 1000: ')
+else:
+    mass = []
+    for i in range(x + y):
+        if i == (x * i - y)**0.5:
+            mass.append(i)
+    print(*mass if len(mass) == 2 else mass + mass)
